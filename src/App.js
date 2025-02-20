e709e66bcd16b70db0c03d6954d34684ba16e86b
muh-younus <braq369@gmail.com>
2025-02-20 15:31:47 +0500

    another commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,44 @@
 import React, { Component } from 'react'
 import Navbar from './components/Navbar'
 import News from './components/News'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Routes
+} from "react-router-dom";
 
 
 
 export default class App extends Component {
   render() {
     return (
-      <div>
+      <>
+        <div>
 
-      <Navbar/>
-      <News pagesize ={6} country="us" category="science" />
-        
-      </div>
+          <Router>
+
+            <Navbar />
+            <News pagesize={6} country="us" category="science" />
+
+            <Routes>
+              <Route exact path="/" element={< News pagesize={6} country="us" category="general" />} />
+              <Route exact  path="/business" element={<News key="business" pagesize={6} country="us" category="bussiness" />} />
+              <Route exact path="/science" element={<News key="science" pagesize={6} country="us" category="science" />} />
+              <Route exact path="/general" element={<News key="general" pagesize={6} country="us" category="general" />} />
+              <Route exact path="/sports" element={<News key="sports" pagesize={6} country="us" category="sports" />} />
+              <Route exact path="/general" element={<News key="general" pagesize={6} country="us" category="general" />} />
+              <Route exact path="/entertainment" element={<News key="entertainment" pagesize={6} country="us" category="entertainment" />} />
+
+
+
+            </Routes>
+
+          </Router>
+
+        </div>
+      </>
     )
   }
 }
